b3b0203b4ca22337761cff5b494fecb400d6a6c2
Emmanuel <jospag4christ@yahoo.com>
2021-11-26 01:28:40 +0100

    modified

--- practise/Book.java
+++ practise/Book.java
@@ -1,2 +1,28 @@
-package practise;public enum Book {
+package practise;
+
+public enum Book {
+
+
+    JHTP("Java How to Program", "2018"),
+    CHTP("C How to Program", "2016"),
+    IW3HTP("Internet & World Wide Web How to Program", "2012"),
+    CPPHTP("C++ How to Program", "2017"),
+    VBHTP("Visual Basic How to Program", "2014"),
+    CSHARPHTP("Visual C# How to Program", "2017");
+
+    private String title;
+    private final String copyRightYear;
+
+    Book(String title, String copyRightYear) {
+        this.title = title;
+        this.copyRightYear = copyRightYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCopyRightYear(){
+        return copyRightYear;
+    }
 }
