7b79330849cdb66c9abcdd2b79874fcf0b84d146
Emmanuel <jospag4christ@yahoo.com>
2021-11-26 01:30:32 +0100

    modified

--- practise/EmployeeDetailsTest.java
+++ practise/EmployeeDetailsTest.java
@@ -1,2 +1,12 @@
-package practise;public class EmployeeDetailsTest {
+package practise;
+
+public class EmployeeDetailsTest {
+    public static void main(String[] args) {
+        Date birthDate = new Date(18, 9, 1991);
+        Date hiredate = new Date(9, 7, 2022);
+        EmployeeDetails employeeDetails = new EmployeeDetails("Emmanuel",
+                "Olorunnishola", birthDate, hiredate);
+
+        System.out.println(employeeDetails);
+    }
 }
